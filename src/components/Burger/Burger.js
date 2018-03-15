b5c041194bb3c9d7c829141c59148054d8eccade
abequinonez <abequinonez@users.noreply.github.com>
2018-03-14 18:55:37 -0700

    feat: Output burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,31 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  /*
+  Declare an ingredients array that will hold the BurgerIngredient
+  components.
+  */
+  let ingredients = [];
+
+  // Loop through every key in the props.ingredients object
+  for (const ingKey in props.ingredients) {
+    /*
+    For every key, use a for loop that iterates as many times as the value
+    contained in each key. For every iteration, add a BurgerIngredient to the
+    ingredients array.
+    */
+    for (let i = 0; i < props.ingredients[ingKey]; i++) {
+      ingredients.push(<BurgerIngredient key={ingKey + i} type={ingKey} />);
+    }
+  }
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,22 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 1,
+        bacon: 1,
+        cheese: 1,
+        meat: 1
+      }
+    };
+  }
+
   render() {
     return (
       <Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Fragment>
     );
